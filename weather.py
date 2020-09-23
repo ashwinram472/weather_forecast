9f4d575d6b5f99e279491c18850ce7570457b612
ashwin <ashwinram472@gmail.com>
2020-09-22 20:23:58 -0400

    'comment'

--- weather.py
+++ weather.py
@@ -1,52 +1,75 @@
+##########################################
+########## ASHWIN BALASUBRAMANIAM ########
+######### PYTHON WEATHER ASSIGNMENT ######
+##########################################
+
+# Importing the required packages
 import requests as re
 import pandas as pd
 import datetime
 import csv
-import os
+from statistics import mean
 
 
 def get_temp(city):
+    # Getting the JSON Data
+    # Enter your API key
+    api_key = ''
     data = re.get('http://api.openweathermap.org/data/2.5/forecast?q=' +
-                  city+'&appid=f6239495a3d267c31944a2930487d78c').json()['list']
+                  city+'‘&units=metric&appid='+api_key).json()['list']
+    # Converting to DataFrame
     df = pd.DataFrame(data)
+    # Converting the Timestamp to datetime format and extracting the day
     df['dt'] = df['dt'].apply(lambda x: (
         datetime.datetime.fromtimestamp(x)).day)
     today = datetime.datetime.now().day
+    # Substting the days from today
     df = df[df['dt'] != today]
-
+    # Appedning the Min and Max temperatures of each day for each 3 hours
     temp_min = []
     temp_max = []
     for i in df['main']:
         d = dict(i)
-        temp_min.append(d['temp_min'] - 273.15)
-        temp_max.append(d['temp_max'] - 273.15)
+        temp_min.append(d['temp_min'])
+        temp_max.append(d['temp_max'])
     df['temp_low'] = temp_min
     df['temp_high'] = temp_max
-
-    df1 = df.groupby('dt').mean()
-    low = df1['temp_low']
-    high = df1['temp_high']
-    f = low.to_list()
-    g = high.to_list()
+    # Aggregating the data to get the minimum and maximum of each day!
+    df1 = df.groupby('dt').agg({'temp_low': 'min', 'temp_high': 'max'})
+    low = df1['temp_low'].to_list()
+    high = df1['temp_high'].to_list()
     temp = []
-    for i, j in zip(f, g):
+    for i, j in zip(low, high):
         temp.append(i)
         temp.append(j)
-    temp.append(min(temp))
-    temp.append(max(temp))
+    # Getting the mean of min and max temperatures
+    temp.append(mean(low))
+    temp.append(mean(high))
     tem = [round(i, 2) for i in temp]
 
     return tem
 
 
 if __name__ == "__main__":
-    print('Hello')
-    n = int(input('Enter number of cities:'))
-    cities = []
-    for i in range(n):
-        print('Enter city ', i, ':')
-        cities.append(input())
 
+    cities = ['Anchorage, USA',
+              'Buenos Aires, Argentina',
+              'São José dos Campos, Brazil',
+              'San José, Costa Rica',
+              'Nanaimo, Canada',
+              'Ningbo, China',
+              'Giza, Egypt',
+              'Mannheim, Germany',
+              'Hyderabad, India',
+              'Tehran, Iran',
+              'Bishkek, Kyrgyzstan',
+              'Riga, Latvia',
+              'Quetta, Pakistan',
+              'Warsaw, Poland',
+              'Dhahran, Saudia Arabia',
+              'Madrid, Spain',
+              'Oldham, England']
+    # Writing to CSV file
     with open('temp.csv', 'w', newline='') as file:
         fields = ['City', 'Min 1', 'Max 1', 'Min 2', 'Max 2',
                   'Min 3', 'Max 3', 'Min 4', 'Max 4', 'Min 5', 'Max 5', 'Min Avg', 'Max Avg']
